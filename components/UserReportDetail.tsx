84cbcbc704bad7152d81e954e9406ffaf5f36fd5
Realzee <128939827+Realzee@users.noreply.github.com>
2026-02-09 09:50:13 +0200

    feat: Update map tiles and attribution
    Switches to CARTO Voyager tiles for a more modern look and feel, and updates attribution to reflect the new tile provider.

--- components/UserReportDetail.tsx
+++ components/UserReportDetail.tsx
@@ -48,12 +48,10 @@ const UserReportDetail: React.FC<{ report: Report, profile: Profile, onEdit: (re
     const [assignmentHistory, setAssignmentHistory] = useState<AssignmentLog[]>([]);
     const { theme } = useTheme();
 
-    const lightMapUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+    const lightMapUrl = "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png";
     const darkMapUrl = "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png";
     const tileUrl = theme === 'dark' ? darkMapUrl : lightMapUrl;
-    const attribution = theme === 'dark' 
-        ? '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>' 
-        : '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+    const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>';
 
     useEffect(() => {
         const fetchDetails = async () => { 
