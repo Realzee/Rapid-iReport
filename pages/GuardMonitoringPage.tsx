7858f9dd499c4cbd42707442c4079117a73c295e
Realzee <128939827+Realzee@users.noreply.github.com>
2026-04-02 03:24:32 +0200

    refactor: Remove unused DetectableEventsFeed component
    The `DetectableEventsFeed` component is no longer used within the `GuardMonitoringPage` and has been removed to streamline the UI and code.

--- pages/GuardMonitoringPage.tsx
+++ pages/GuardMonitoringPage.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import GuardStatusDashboard from '../components/GuardMonitoring/GuardStatusDashboard';
 import GuardMapView from '../components/GuardMonitoring/GuardMapView';
-import DetectableEventsFeed from '../components/GuardMonitoring/DetectableEventsFeed';
 import ConfigurationPanel from '../components/GuardMonitoring/ConfigurationPanel';
 import ReportingPanel from '../components/GuardMonitoring/ReportingPanel';
 import AnalyticsPanel from '../components/GuardMonitoring/AnalyticsPanel';
@@ -32,7 +31,7 @@ const GuardMonitoringPage: React.FC = () => {
                             className={`px-4 py-2 rounded-md text-sm font-medium transition-colors ${
                                 activeTab === tab.id
                                     ? 'bg-white dark:bg-gray-700 text-gray-900 dark:text-white shadow'
-                                    : 'text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white'
+                                    : 'text-gray-600 dark:bg-gray-800 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white'
                             }`}
                         >
                             {tab.label}
@@ -42,14 +41,9 @@ const GuardMonitoringPage: React.FC = () => {
             </div>
 
             {activeTab === 'overview' && (
-                <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-                    <div className="lg:col-span-2 space-y-6">
-                        <GuardStatusDashboard responders={responders} />
-                        <GuardMapView responders={responders} />
-                    </div>
-                    <div className="lg:col-span-1">
-                        <DetectableEventsFeed />
-                    </div>
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+                    <GuardStatusDashboard responders={responders} />
+                    <GuardMapView responders={responders} />
                 </div>
             )}
             {activeTab === 'config' && <ConfigurationPanel />}
